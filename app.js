d9ab822abdde80c959b75e6c699fb3d8d6d8e718
manoj <manojnallusamy1999@gmail.com>
2020-04-07 22:47:49 +0530

    modified app.js

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ app.get("/",(req,res)=>{
         });
     });
 });   
-    
-app.listen(3000,()=>{
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}    
+app.listen(port,()=>{
     console.log("Covid App Server Started!!!");
 })    
